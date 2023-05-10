b36904373b23d9fb80468ed55af7d10e04e428af
DevMigue <127422296+DevMigue@users.noreply.github.com>
2023-05-10 23:47:18 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function dealCard() {
+  var suitTexts = ['♦','♥','♠','♣'];
+  var numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  var randomSuitIndex = Math.floor(Math.random() * suitTexts.length);
+  var randomNumberIndex = Math.floor(Math.random() * numbers.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var cardSuitElement = document.getElementsByClassName('suitSpan');
+  var cardNumberElement = document.getElementById('cardNumber');
+
+  cardNumberElement.textContent = numbers[randomNumberIndex];
+  for (var i = 0; i < cardSuitElement.length; i++) {
+    cardSuitElement[i].textContent = suitTexts[randomSuitIndex];
+    if (suitTexts[randomSuitIndex] === '♦' || suitTexts[randomSuitIndex] === '♥') {
+      cardSuitElement.style.color = "red";
+    }
+  }
+}
